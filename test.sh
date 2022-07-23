653e679f405a3b3e7b336d609390afef1f1b2c06
To grivel <grivel.theo@protonmail.com>
2022-07-23 17:01:27 +0200

    Improved test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/07/23 16:51:30 by melogr@phy        #+#    #+#              #
+#    Updated: 2022/07/23 16:58:23 by melogr@phy       ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 
 ### ARGUMENTS LIST
 # number_of_philosophers
@@ -7,18 +19,38 @@
 # time_to_sleep
 # [number_of_times_each_philosopher_must_eat]
 
-message ()	{
-	echo -e	"\n------\n$1\n-----\n"
+# 1. Comment
+# 2. Argument
+test_philo () {
+
+	printf "# %s\n" "$1"
+	printf "./philo %s\n" "$2"
+	./philo $2
+	echo
+
 }
 
-message "not enought arguments"
-./philo 500 400 200	
+test_usage () {
 
-message	"too much arguments"
-./philo 12 500 400 200 42 50
+	printf "###Test bad usage\n"
+	test_philo "not enought arguments" "500 400 200"
+	test_philo "too much arguments" "12 500 400 200 42 50"
 
-message "number of times each philosopher must eat"
-./philo 12 500 400 200 42
+}
 
-message "infinity"
-./philo 12 500 400 200
+test_normal () {
+
+	printf "###Test normal usage\n"
+	test_philo "number of times each philosopher must eat" "12 500 400 200 42"
+	test_philo "infinity" "12 500 400 200"
+
+}
+
+main () {
+
+	test_usage
+	test_normal
+
+}
+
+main
