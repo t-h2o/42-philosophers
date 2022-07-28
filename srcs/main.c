7978a6564950d2bcad151a4864cdb3e64ff37e6a
To grivel <grivel.theo@protonmail.com>
2022-07-28 16:46:00 +0200

    Updated comments line main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,24 @@
 /*   By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 15:55:23 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/07/28 16:28:54 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/07/28 16:45:18 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"philo.h"
 
-//		MAIN
-//	1. parse arguments
-//	2. init philo
-//	3. init supervisor
-//	4. start time
-//	5. start simulation
+// 0. parse arguments
+// 1. init philo
+// 2. init supervisor
+// 3. start time
+// 4. start simulation
 //
-//		TIMES
-//	times[0]	: number of philosophers
-//	times[1]	: time to die
-//	times[2]	: time to eat
-//	times[3]	: time to sleep
-//	(times[4])	: number of times each philosopher must eat
+//  TIMES
+//  times[0] : number of philosophers
+//  times[1] : time to die
+//  times[2] : time to eat
+//  times[3] : time to sleep
+// ?times[4] : number of times each philosopher must eat
 int	main(int argc, char **argv)
 {
 	(void)argc;
