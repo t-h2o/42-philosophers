182b5acae9ef688a5898f4c32b38677dd74af82e
To grivel <grivel.theo@protonmail.com>
2022-07-28 15:53:10 +0200

    Created philo.h

---
+++ headers/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nasackers <hack@nasa.usa4all>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 00:31:05 by nasackers         #+#    #+#             */
+/*   Updated: 2022/07/28 15:52:54 by melogr@phy       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+//	HEADERS
+
+# include	<stdio.h>
+# include	<stdlib.h>
+# include	<sys/time.h>
+# include	<unistd.h>
+# include	<pthread.h>
+
+#endif	/*	PHILO_H	*/
