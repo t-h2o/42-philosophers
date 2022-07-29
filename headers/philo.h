12fe1154097f17d891538d3441f7cee0b01763cb
To grivel <grivel.theo@protonmail.com>
2022-07-29 14:13:20 +0200

    Created putnbr_fd

--- headers/philo.h
+++ headers/philo.h
@@ -6,7 +6,7 @@
 /*   By: nasackers <hack@nasa.usa4all>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 00:31:05 by nasackers         #+#    #+#             */
-/*   Updated: 2022/07/29 12:58:44 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/07/29 13:59:29 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		parsing(int argc, char **argv, int infos[5]);
 
 // UTILS STRINGS
 
+void	putnbr_fd(int number, int fd);
 void	putstr_fd(char *str, int fd);
 void	print_error(char *msg);
 
--- srcs/utils_str.c
+++ srcs/utils_str.c
@@ -6,11 +6,27 @@
 /*   By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:16:49 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/07/28 16:30:55 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/07/29 14:07:54 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"philo.h"
+
+void	putnbr_fd(int number, int fd)
+{
+	char	digit;
+
+	if (number >= 10)
+	{
+		putnbr_fd(number / 10, fd);
+		putnbr_fd(number % 10, fd);
+	}
+	else
+	{
+		digit = number + '0';
+		write(fd, &digit, 1);
+	}
+}
 
 // Print str (string) in fd (file descriptor)
 void	putstr_fd(char *str, int fd)
