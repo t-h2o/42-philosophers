6bcaad97fd83df6be973a441a4c0b8ca95fecd1f
Theo Grivel <tgrivel@c3r1s5.42lausanne.ch>
2022-08-02 16:21:09 +0200

    Get from my old philo
    Signed-off-by: Theo Grivel <tgrivel@c3r1s5.42lausanne.ch>

--- srcs/time.c
+++ srcs/time.c
@@ -6,11 +6,31 @@
 /*   By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 12:27:57 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/08/02 12:29:25 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/08/02 16:20:08 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"philo.h"
+
+//	set start time
+void
+	time_start(struct timeval *start)
+{
+	gettimeofday(start, 0);
+}
+
+//	return current time [ms]
+unsigned int
+	time_now(struct timeval *start)
+{
+	unsigned int	now;
+	struct			timeval time;
+
+	gettimeofday(&time, 0);
+	now = (unsigned int)(((time.tv_sec - start->tv_sec) * 1000) \
+		+ ((time.tv_usec - start->tv_usec) / 1000));
+	return (now);
+}
 
 uint64_t
 	get_time(void)
