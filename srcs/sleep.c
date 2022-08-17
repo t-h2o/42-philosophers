0a758d7b5eda83d6c912f6db4180316c647f6ccd
To grivel <grivel.theo@protonmail.com>
2022-08-17 21:48:49 +0200

    Removed useless data

--- srcs/sleep.c
+++ srcs/sleep.c
@@ -6,7 +6,7 @@
 /*   By: melogr@phy <tgrivel@student.42lausanne.ch  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 00:37:36 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/08/17 17:01:43 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/08/17 21:48:14 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@
 // 2. Wait the time's sleep
 int	p_sleep(t_philo *philo)
 {
-	int	begin;
-	int	now;
-
-	begin = time_now(&(philo->info->start));
-	now = begin;
-	msg_philo(" is sleeping\n", philo, begin);
+	msg_philo(" is sleeping\n", philo, time_now(&(philo->info->start)));
 	return (my_sleep(philo, philo->info->args[3]));
 }
